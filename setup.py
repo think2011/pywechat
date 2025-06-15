513b0d3addb6b2e5f7c70d03b9417c3e60bdc99e
Hectic  coder <161811788+Hello-Mr-Crab@users.noreply.github.com>
2025-06-16 02:09:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup,find_packages
 setup(name='pywechat127',
-version='1.8.8',
+version='1.9.6',
 author='Hello-Mr-Crab',
 author_email='3083256475@qq.com',
 description=f'A Powerful Windows-PC-Wechat automation Tool',
@@ -9,8 +9,8 @@ long_description_content_type='text/markdown',
 url='https://github.com/Hello-Mr-Crab/pywechat',
 packages=find_packages(),
 license='Apache-2.0',
-keywords=['wechat','python','windows','automation'],
+keywords=['rpa','wechat','windows','automation'],
 install_requires=[
-'PyAutoGUI>=0.9.54','pycaw>=20240210','pywin32>=308','pywin32-ctypes>=0.2.2','pywinauto>=0.6.8','pillow>=11.1.0']
+'PyAutoGUI>=0.9.54','pycaw>=20240210','pywin32>=308','pywin32-ctypes>=0.2.2','pywinauto>=0.6.8','pillow>=11.1.0','psutil>=7.0.0']
 )
 
